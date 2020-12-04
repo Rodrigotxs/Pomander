faff671a6746c1eaa4bd8b6c9873745d163c314d
unknown <rodrigo.teixeira_santos@hotmail.com>
2020-12-04 13:23:53 -0300

    Pomander Backup

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+import { Post } from './post/post.model';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(
+      (x) => (this.currentUser = x)
+    );
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  posts: Post[] = [];
+  onPostAdicionado(post) {
+    this.posts = [...this.posts, post];
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,58 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { PostHomeComponent } from './post/post-home/post-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostAdminComponent } from './post/post-admin/post-admin.component';
+
+import { PostService } from './post/post.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PostHomeComponent,
+    PostAdminComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    PostService,
+
+    // provider used to create fake backend
+    fakeBackendProvider,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
